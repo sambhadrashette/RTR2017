205db14379c8e0e47e606f84d0bfd3be64952e72
Sachin Bhadrashette <sambhadrashette@users.noreply.github.com>
2017-07-19 22:32:43 +0530

    Solution for 8thJuly_Shapes_Kundli_And_Other assignments

---
+++ FixFunctionPipeline/Native/05_8thJuly_Shapes_Kundli_And_Other/01_CodeSplitIntoFunctions/Program_3_RectangleOverGraph/Program_7_RectangleOverGraph/RectangleOverGraph.cpp
@@ -0,0 +1,313 @@
+#include<windows.h>
+#include<gl/GL.h>
+
+#define WIN_WIDTH 800
+#define WIN_HEIGHT 600
+
+#pragma comment(lib, "opengl32.lib")
+
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+
+HWND ghwnd = NULL;
+HDC ghdc = NULL;
+HGLRC ghrc = NULL;
+
+DWORD dwStyle;
+WINDOWPLACEMENT wPrev = { sizeof(WINDOWPLACEMENT) };
+
+
+bool gbActiveWindow = false;
+bool gbEscapeKeyPressed = false;
+bool gbFullScreen = false;
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdLine, int iCmdShow) {
+	void display(void);
+	void initialize(void);
+	void uninitialize(void);
+
+	WNDCLASSEX wndClass;
+	HWND hwnd;
+	MSG msg;
+	TCHAR szClassName[] = TEXT("VRedLine");
+	bool bDone = false;
+	wndClass.cbSize = sizeof(WNDCLASSEX);
+	wndClass.style = CS_HREDRAW | CS_VREDRAW | CS_OWNDC;
+	wndClass.cbClsExtra = 0;
+	wndClass.cbWndExtra = 0;
+	wndClass.hInstance = hInstance;
+	wndClass.hbrBackground = (HBRUSH)GetStockObject(BLACK_BRUSH);
+	wndClass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	wndClass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wndClass.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
+	wndClass.lpfnWndProc = WndProc;
+	wndClass.lpszClassName = szClassName;
+	wndClass.lpszMenuName = NULL;
+
+	RegisterClassEx(&wndClass);
+
+	hwnd = CreateWindowEx(WS_EX_APPWINDOW,
+		szClassName,
+		TEXT("RollNo 74 | Sachin Bhadrashette | Rectangle over GraphPaper"),
+		WS_OVERLAPPEDWINDOW | WS_CLIPCHILDREN | WS_CLIPSIBLINGS | WS_VISIBLE,
+		100,
+		100,
+		WIN_WIDTH,
+		WIN_HEIGHT,
+		NULL,
+		NULL,
+		hInstance,
+		NULL);
+
+	ghwnd = hwnd;
+
+	initialize();
+
+	ShowWindow(hwnd, SW_SHOW);
+	SetForegroundWindow(hwnd);
+	SetFocus(hwnd);
+
+	while (bDone == false) {
+		if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
+		{
+			if (msg.message == WM_QUIT)
+				bDone = true;
+			else {
+				TranslateMessage(&msg);
+				DispatchMessage(&msg);
+			}
+		}
+		else {
+			if (gbActiveWindow == true)
+			{
+				if (gbEscapeKeyPressed == true) {
+					bDone = true;
+				}
+				display();
+			}
+		}
+	}
+
+	uninitialize();
+	return ((int)msg.wParam);
+}
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam) {
+
+	void resize(int, int);
+	void ToggleFullScreen(void);
+	void uninitialize(void);
+
+	switch (iMsg) {
+	case WM_ACTIVATE:
+		if (HIWORD(wParam) == 0)
+			gbActiveWindow = true;
+		else
+			gbActiveWindow = false;
+		break;
+	case WM_SIZE:
+		resize(LOWORD(lParam), HIWORD(lParam));
+		break;
+	case WM_KEYDOWN:
+		switch (wParam)
+		{
+		case VK_ESCAPE:
+			gbEscapeKeyPressed = true;
+			break;
+		case 0x46:
+			if (gbFullScreen == false) {
+				ToggleFullScreen();
+				gbFullScreen = true;
+			}
+			else {
+				ToggleFullScreen();
+				gbFullScreen = false;
+			}
+			break;
+		default: break;
+		}
+		break;
+	case WM_LBUTTONDOWN:
+		break;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	default:
+		break;
+	}
+	return (DefWindowProc(hwnd, iMsg, wParam, lParam));
+}
+
+void ToggleFullScreen() {
+	MONITORINFO mi;
+	dwStyle = GetWindowLong(ghwnd, GWL_STYLE);
+	if (gbFullScreen == FALSE) {
+		if (dwStyle & WS_OVERLAPPEDWINDOW) {
+			wPrev.length = sizeof(WINDOWPLACEMENT);
+			BOOL bIsWindowPlacement = GetWindowPlacement(ghwnd, &wPrev);
+			HMONITOR hMonitor = MonitorFromWindow(ghwnd, MONITORINFOF_PRIMARY);
+			mi.cbSize = sizeof(MONITORINFO);
+			BOOL bIsMonitorInfo = GetMonitorInfo(hMonitor, &mi);
+
+			if (bIsWindowPlacement == TRUE && bIsMonitorInfo == TRUE) {
+				SetWindowLong(ghwnd, GWL_STYLE, dwStyle & ~WS_OVERLAPPEDWINDOW);
+				SetWindowPos(ghwnd, HWND_TOP,
+					mi.rcMonitor.left,
+					mi.rcMonitor.top,
+					mi.rcMonitor.right - mi.rcMonitor.left,
+					mi.rcMonitor.bottom - mi.rcMonitor.top,
+					SWP_NOZORDER | SWP_FRAMECHANGED);
+				ShowCursor(false);
+			}
+		}
+	}
+	else {
+		SetWindowLong(ghwnd, GWL_STYLE, dwStyle | WS_OVERLAPPEDWINDOW);
+		SetWindowPlacement(ghwnd, &wPrev);
+		SetWindowPos(ghwnd, HWND_TOP,
+			0,
+			0,
+			0,
+			0,
+			SWP_NOZORDER | SWP_FRAMECHANGED | SWP_NOMOVE | SWP_NOSIZE | SWP_NOOWNERZORDER);
+		ShowCursor(true);
+	}
+}
+
+void initialize() {
+	void resize(int, int);
+	PIXELFORMATDESCRIPTOR pfd;
+	int iPixelFormatIndex;
+
+	ZeroMemory(&pfd, sizeof(PIXELFORMATDESCRIPTOR));
+
+	pfd.nSize = sizeof(PIXELFORMATDESCRIPTOR);
+	pfd.nVersion = 1;
+	pfd.dwFlags = PFD_DRAW_TO_WINDOW | PFD_SUPPORT_OPENGL | PFD_DOUBLEBUFFER;
+	pfd.iPixelType = PFD_TYPE_RGBA;
+	pfd.cColorBits = 32;
+	pfd.cRedBits = 8;
+	pfd.cGreenBits = 8;
+	pfd.cBlueBits = 8;
+	pfd.cAlphaBits = 8;
+
+	ghdc = GetDC(ghwnd);
+	iPixelFormatIndex = ChoosePixelFormat(ghdc, &pfd);
+
+	if (iPixelFormatIndex == 0) {
+		ReleaseDC(ghwnd, ghdc);
+		ghdc = NULL;
+	}
+
+	if (SetPixelFormat(ghdc, iPixelFormatIndex, &pfd) == FALSE) {
+		ReleaseDC(ghwnd, ghdc);
+		ghdc = NULL;
+	}
+
+	ghrc = wglCreateContext(ghdc);
+	if (ghrc == NULL) {
+		ReleaseDC(ghwnd, ghdc);
+		ghdc = NULL;
+	}
+	if (wglMakeCurrent(ghdc, ghrc) == FALSE) {
+		wglDeleteContext(ghrc);
+		ghrc = NULL;
+		ReleaseDC(ghwnd, ghdc);
+		ghdc = NULL;
+	}
+
+	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+}
+
+void display() {
+	glClear(GL_COLOR_BUFFER_BIT);
+	glMatrixMode(GL_MODELVIEW);
+	void drawGrid();
+	void drawRectangle();
+	void drawXYAxis();
+	glLoadIdentity();
+	glLineWidth(1.0f);
+	drawGrid();
+	drawXYAxis();
+	glLineWidth(3.0f);
+	drawRectangle();
+	SwapBuffers(ghdc);
+}
+
+void drawRectangle() {
+	glBegin(GL_LINES);
+	glColor3f(1.0f, 1.0f, 0.0f);
+	glVertex3f(-0.5f, 0.5f, 0.0f);
+	glVertex3f(-0.5f, -0.5f, 0.0f);
+	glVertex3f(-0.5f, -0.5f, 0.0f);
+	glVertex3f(0.5f, -0.5f, 0.0f);
+	glVertex3f(0.5f, -0.5f, 0.0f);
+	glVertex3f(0.5f, 0.5f, 0.0f);
+	glVertex3f(0.5f, 0.5f, 0.0f);
+	glVertex3f(-0.5f, 0.5f, 0.0f);
+	glEnd();
+}
+
+void drawGrid() {
+	glBegin(GL_LINES);
+	glColor3f(0.0f, 0.0f, 1.0f);
+	GLfloat axis = 1;
+	for (int i = 1; i <= 40; i++) {
+		axis = (GLfloat)(axis - 0.05);
+		//verticle lines
+		glVertex3f(axis, 1.0f, 0.0f);
+		glVertex3f(axis, -1.0f, 0.0f);
+
+		//horizontal lines
+		glVertex3f(1.0f, axis, 0.0f);
+		glVertex3f(-1.0f, axis, 0.0f);
+	}
+	glEnd();
+}
+
+void drawXYAxis() {
+	glLineWidth(3.0f);
+	//Centre Gree and red line
+	glBegin(GL_LINES);
+	//Horizontal Red line
+	glColor3f(1.0f, 0.0f, 0.0f);
+	glVertex3f(1.0f, 0.0f, 0.0f);
+	glVertex3f(-1.0f, 0.0f, 0.0f);
+
+	//Verticle green line
+	glColor3f(0.0f, 1.0f, 0.0f);
+	glVertex3f(0.0f, 1.0f, 0.0f);
+	glVertex3f(0.0f, -1.0f, 0.0f);
+	glEnd();
+}
+
+void resize(int width, int height) {
+	if (height == 0) {
+		height = 1;
+	}
+	glViewport(0, 0, (GLsizei)width, (GLsizei)height);
+}
+
+void uninitialize(void) {
+	if (gbFullScreen == true) {
+		dwStyle = GetWindowLong(ghwnd, GWL_STYLE);
+		SetWindowLong(ghwnd, GWL_STYLE, dwStyle | WS_OVERLAPPEDWINDOW);
+		SetWindowPlacement(ghwnd, &wPrev);
+		SetWindowPos(ghwnd, HWND_TOP, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE | SWP_NOOWNERZORDER | SWP_NOZORDER | SWP_FRAMECHANGED);
+	}
+
+	wglMakeCurrent(NULL, NULL);
+
+	wglDeleteContext(ghrc);
+
+	ghrc = NULL;
+
+	ReleaseDC(ghwnd, ghdc);
+	ghdc = NULL;
+
+	DestroyWindow(ghwnd);
+	ghwnd = NULL;
+
+}
+
+
+
